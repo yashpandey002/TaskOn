715a0db935ff567d3dfdb79a1bf2fe99dd3bc34a
yashpandey002 <pandeyyash002@gmail.com>
2023-01-30 01:39:53 +0530

    add task component added

--- src/app/app.js
+++ src/app/app.js
@@ -79,7 +79,9 @@ const colorPallete = document.querySelector('.list__set-color__pallete');
 
 colorPallete.addEventListener('input', () => {
     const list = document.querySelector('.list');
-    list.style.borderColor = colorPallete.value;
+    list.style.borderLeftWidth = '1rem';
+    list.style.borderLeftStyle = 'solid';
+    list.style.borderLeftColor = colorPallete.value;
 });
 
 listSetColorBtn.addEventListener('click', () => {
@@ -89,5 +91,73 @@ listSetColorBtn.addEventListener('click', () => {
 document.addEventListener('click', (e) => {
     if (e.target === colorPalleteBG) {
         colorPalleteBG.classList.add('hide');
+    }
+});
+
+// Add task
+const addTaskText = document.querySelector('.list__add-task__text-box');
+const taskInputEl = document.querySelector('.list__add-task__input');
+
+const addTask = function () {
+    if (taskInputEl.value === '') {
+        return;
+    } else {
+        const listTask = document.querySelector('.list__tasks');
+        listTask.classList.remove('hide');
+
+        const tasksContainer = document.querySelector(
+            '.list__tasks__items-container'
+        );
+
+        const newTaskLi = document.createElement('li');
+        newTaskLi.classList.add('list__tasks__item', 'flex');
+        tasksContainer.append(newTaskLi);
+
+        const newTaskIconContainer = document.createElement('div');
+        newTaskIconContainer.classList.add(
+            'list__tasks__indicator-icon-box',
+            'flex'
+        );
+        newTaskLi.append(newTaskIconContainer);
+
+        const newTaskIcon = document.createElement('div');
+        newTaskIcon.classList.add('list__tasks__indicator-icon');
+        newTaskIconContainer.append(newTaskIcon);
+
+        const newTaskName = document.createElement('div');
+        newTaskName.classList.add('list__tasks__name');
+        newTaskName.textContent = taskInputEl.value;
+        newTaskLi.append(newTaskName);
+
+        const newTaskEditIconContainer = document.createElement('div');
+        newTaskEditIconContainer.classList.add(
+            'list__tasks__edit-task-icon-box',
+            'hide'
+        );
+        newTaskLi.append(newTaskEditIconContainer);
+
+        taskInputEl.value = '';
+    }
+};
+
+taskInputEl.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.key === 'Escape') {
+        addTask();
+    }
+});
+
+document.addEventListener('click', (e) => {
+    const element = e.target;
+    if (element.closest('.list__add-task')) {
+        addTaskText.classList.add('hide');
+        taskInputEl.classList.remove('hide');
+        taskInputEl.focus();
+    } else {
+        if (taskInputEl.value != '') {
+            addTask();
+        }
+        addTaskText.classList.remove('hide');
+        taskInputEl.classList.add('hide');
+        taskInputEl.value = '';
     }
 });
